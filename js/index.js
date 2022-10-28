c1881a257d8678298b93b12f64aaeea773411894
kirran01 <Kirran-@live.com>
2022-10-28 11:43:04 -0400

    iteration 1 & 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 700;
+const backgroundImg = new Image(); // Create new <img> element
+backgroundImg.src = "./images/road.png"; // Set source path
+const carImg = new Image();
+carImg.src = "./images/car.png";
+console.log(backgroundImg);
+
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(carImg, canvas.width / 2 - 25, canvas.height - 80, 50, 75);
+  }
 };
