2b6af0b7f285d3e6df804d1439b343c085914c5e
kirran01 <Kirran-@live.com>
2022-10-30 00:31:48 -0400

    obstacles fly down either side of the canvas

--- js/index.js
+++ js/index.js
@@ -8,9 +8,11 @@ const carImg = new Image();
 carImg.src = "./images/car.png";
 
 class Car {
-  constructor(x, y) {
+  constructor(x, y, width, height) {
     this.carXPosition = x;
     this.carYposition = y;
+    this.width = width;
+    this.height = height;
   }
 
   moveLeft() {
@@ -21,15 +23,23 @@ class Car {
     this.carXPosition += 10;
   }
 
-  //draw method is needed as the car will be continuously drawn every frame, this method will take the dynamic 
-  //position coordinates of the car. 
-
+  //draw method is needed as the car will be continuously drawn every frame, this method will take the dynamic
+  //position coordinates of the car.
 
   drawCar() {
     ctx.drawImage(carImg, this.carXPosition, this.carYposition, 50, 75);
   }
 }
 
+class Wall extends Car {
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+  }
+
+  wallMoveDown() {
+    this.carYposition += 5;
+  }
+}
 
 //initial position is declared with new keyword
 const car = new Car(canvas.width / 2 - 25, canvas.height - 80);
@@ -41,7 +51,6 @@ window.addEventListener("keydown", (e) => {
   switch (e.code) {
     case "ArrowLeft":
       car.moveLeft();
-      console.log(car.carXPosition);
       break;
 
     case "ArrowRight":
@@ -50,13 +59,29 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
+let framecount = 0;
+let wallStorage = [];
+
 let animationLoop = () => {
-  console.log("loop running");
+  framecount++;
+
+  if (framecount % 60 == 0) {
+    let leftWall = new Wall(0, 10, 100, 10);
+    let rightWall = new Wall(canvas.width-50, 10, -100, 10);
+    wallStorage.push(leftWall);
+    wallStorage.push(rightWall)
+  }
+
   clearCanvas();
-  ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height); //draws the background img
+
+  for (let i = 0; i < wallStorage.length; i++) {
+    wallStorage[i].wallMoveDown();
+    wallStorage[i].drawCar();
+  }
+
   car.drawCar();
 };
-
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
